03e942a6458ab38d4f28165e61dd8b2e1fe6ca1d
tomasmatus <tomatus777@gmail.com>
2025-05-23 18:01:29 +0200

    use blink-cmp-copilot

---
+++ lua/custom/plugins/blink-cmp-copilot.lua
@@ -0,0 +1,8 @@
+-- Github copilot-cmp
+-- Moves suggestions into completions menu
+-- https://github.com/giuxtaposition/blink-cmp-copilot
+-- see the link above for blink-cmp configuration
+
+return {
+  "giuxtaposition/blink-cmp-copilot",
+}
--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -31,6 +31,7 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
+      "giuxtaposition/blink-cmp-copilot",
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -76,9 +77,15 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'copilot' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            score_offset = 100,
+            async = true,
+          }
         },
       },
 
